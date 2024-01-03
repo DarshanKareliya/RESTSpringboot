b7faf080db6673c48b816ff6e9acf95130ff2c3b
DarshanKareliya <kareliyadarshan121@gmail.com>
2024-01-03 15:17:51 +0530

    reoved unwanted code for annotation based auth

--- src/main/java/com/Springboot/SpringbootApp/auth/SecurityConfig.java
+++ src/main/java/com/Springboot/SpringbootApp/auth/SecurityConfig.java
@@ -25,10 +25,10 @@ public class SecurityConfig {
     public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
 
         http.csrf(AbstractHttpConfigurer::disable)
-                .authorizeHttpRequests(request->
+               /* .authorizeHttpRequests(request->
                 {
                     request.anyRequest().permitAll();
-                })
+                })*/
                 .sessionManagement(manager->
                         manager.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authenticationProvider(authenticationProvider)
